d4cbbb4df2d77681fc03385d7d8840344b0ef14b
ermaccw <wcai@live.cn>
2020-02-13 13:00:32 +0800

    测试修改

--- main.cpp
+++ main.cpp
@@ -7,6 +7,9 @@ int main()
     using std::cout;
     Stock stock1;
     stock1.show();
+    Stock stock2("User1", 15, 23.6);
+    stock2.buy(3, 10.6);
+    stock2.show;
     system("pause");
     return 0;
 }
