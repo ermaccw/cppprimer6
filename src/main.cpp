f38d723a338e28c66ff749697a7396dea06ac009
ermaccw <wcai@live.cn>
2020-02-29 17:27:24 +0800

    编程练习10-7

--- src/main.cpp
+++ src/main.cpp
@@ -1,58 +1,24 @@
-//main.cpp
-//从栈中添加和删除customer结构。每次customer结构被删除时，其payment的值都被加入到总数中，并报告总数。应可以直接使用Stack类而不作修改；
-//只需修改typedef声明，使item的类型为customer，而不是unsigned long皆可。
-#include "stack.h"
+//betelgeusean_plorg -- main.cpp
+#include "betelgeusean_plorg.h"
 #include <iostream>
-#include <cctype>
 
 int main()
 {
-    using namespace std;
-    Stack st;
-    char ch;
-    customer po;
-    cout << "Please enter A to add a purchase order,\n"
-         << "P to process a PO, or Q to quit.\n";
-    while (cin >> ch && toupper(ch) != 'Q')
-    {
-        while (cin.get() != '\n')
-            continue;
-        if (!isalpha(ch))
-        {
-            cout << "\a";
-            continue;
-        }
-        switch (ch)
-        {
-        case 'A':
-        case 'a':
-            cout << "Enter a PO username to add: ";
-            cin.getline(po.fullname, 35);
-            cout << "Enter a PO number to add: ";
-            cin >> po.payment;
-            cin.get();
-            if (st.isfull())
-                cout << "stack already full\n";
-            else
-                st.push(po);
-            break;
-        case 'P':
-        case 'p':
-            if (st.isempty())
-                cout << "stack already empty\n";
-            else
-            {
-                st.pop(po);
-                cout << "PO #" << po.fullname << " "
-                     << po.payment << " popped\n";
-            }
-            break;
-        }
-        cout << "Please enter A to add a purchase order,\n"
-             << "P to process a PO, or Q to quit.\n";
-    }
-    cout << "Bye\n";
+    Plorg p1;
+    std::cout << "P1 infomation# ";
+    p1.show();
 
+    Plorg p2("user1", 25);
+    std::cout << "P2 infomation# ";
+    p2.show();
+
+    p1.setCI(30);
+    std::cout << "P1 set CI# ";
+    p1.show();
+
+    std::cout << "P3 infomation# ";
+    Plorg p3("hfaohfoahfohafafaffgad", 20);
+    p3.show();
     system("pause");
     return 0;
 }
